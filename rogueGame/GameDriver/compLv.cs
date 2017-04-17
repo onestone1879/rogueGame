676394fe1c70d852d92f0a518535c3b133e1b1f4
BorisSonton <onestone1879@msn.cn>
2017-04-18 00:26:18 +0800

    1

---
+++ rogueGame/GameDriver/compLv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDriver
+{
+    class compLv
+    {
+        public static int getNeedExp(int lv)
+        {
+            double p2 = 50.448;
+            double p1 = -109.38;
+            double p0 = 450;
+            int exp = (int)(Math.Round(p2*lv*lv+p1*lv+p0) * 100);
+            return exp;
+        } 
+    }
+}
---
+++ rogueGame/GameDriver/player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDriver
+{
+    class player
+    {
+        // gold
+        private int gold;
+        public int getGold()
+        {
+            return gold;
+        }
+        public int addGold(int add)
+        {
+            gold += add;
+            return gold;
+        }
+        public bool spentGold(int sp)
+        {
+            if (gold < sp)
+                return false;
+            gold -= sp;
+            return true;
+        }
+
+        // exp
+        private int exp;
+        public int getExp()
+        {
+            return exp;
+        }
+        public int addExp(int add)
+        {
+            exp += add;
+            return exp;
+        }
+
+        // exp to lv up
+        private int elu;
+        public int getElu()
+        {
+            return elu;
+        }
+
+        // lv
+        private int lv;
+        public int getLv()
+        {
+            return lv;
+        }       
+    }
+}
