d344fd0305d82aeabd5d68aa9a8cc7c56bd1a304
Arrson <boris1990@126.com>
2017-04-22 00:08:11 +0800

    abstract spirit class

--- rogueGame/GameDriver/spirit.cs
+++ rogueGame/GameDriver/spirit.cs
@@ -6,74 +6,46 @@ using System.Threading.Tasks;
 
 namespace GameDriver
 {
-    public class spirit
+    public class spirit : spiritAb
     {
-        private int lv;
-        private int aStr;
-        private int aDex;
-        private int aInt;
-        private int aVil;
-        private static Random r = new Random();
+        public spirit(int level) : base(level) { }
+        public spirit(int level, int str, int dex, int inte, int vil) : base(level, str, dex, inte, vil) { }
 
-        public spirit(int level)
-        {
-            lv = level;
-            aStr = 0;
-            aDex = 0;
-            aInt = 0;
-            aVil = 0;
-        }
-
-        public int getLv()
-        {
-            return lv;
-        }
-
-        public int getStr()
+        public override int getStr()
         {
             return aStr + (int)(lv * 1.2);
         }
-        public int getDex()
+        public override int getDex()
         {
             return aDex + (int)(lv * 1.2);
         }
-        public int getInt()
+        public override int getInt()
         {
             return aInt + (int)(lv * 1.2);
         }
-        public int getVil()
+        public override int getVil()
         {
             return aVil + (int)(lv * 2);
         }
-
-        public int getMaxHealth()
+        public override int getMaxHealth()
         {
             return lv * 20 + getVil() * 10;
         }
-        public int getAtk()
+        public override int getAtk()
         {
             return (int)(getStr() * 2);
         }
-        public int getAmr()
+        public override int getAmr()
         {
             return (int)(getDex() * 1.3);
         }
-        public int getDod()
+        public override int getDod()
         {
             return (int)(getInt() * 1.3);
         }
-        public double getSpd()  // 正整数，一般为4~11，表示多少周期（100ms）攻击一次
+        public override double getSpd()  // 正整数，一般为4~11，表示多少周期（100ms）攻击一次
         {
             return Math.Round(10.0 / (Math.Log((double)getDex() / (double)lv + 1.0) + 0.17));
-        }
-
-        public int compDmg(spirit tg)
-        {
-            double adj = Math.Exp(0.03 * (lv - tg.lv));
-            double luk = r.NextDouble() / 3.5 + 0.86;
-            int dmg = (int)((getAtk() - tg.getAmr()) * adj * luk);
-            if (dmg < 1) dmg = 1;
-            return dmg;
         }
     }
 }
---
+++ rogueGame/GameDriver/spiritAb.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDriver
+{
+    public abstract class spiritAb
+    {
+        protected int lv = 1;
+        protected int aStr = 0;
+        protected int aDex = 0;
+        protected int aInt = 0;
+        protected int aVil = 0;
+        protected int health;
+        protected static Random r = new Random();
+
+        public spiritAb(int level)
+        {
+            lv = level;
+            health = getMaxHealth();
+        }
+        public spiritAb(int level, int str, int dex, int inte, int vil)
+        {
+            aStr = str;
+            aDex = dex;
+            aInt = inte;
+            aVil = vil;
+            lv = level;
+            health = getMaxHealth();
+        }
+
+        public int getLv()
+        {
+            return lv;
+        }
+        public int lvUP()
+        {
+            return ++lv;
+        }
+        public int getHealth()
+        {
+            return health;
+        }
+        public int getDmg(int dmg)
+        {
+            if (dmg < 0)
+                return health;
+            health -= dmg;
+            if (health < 0)
+                health = 0;
+            return health;
+        }
+        public int getHeal(int heal)
+        {
+            if (heal < 0)
+            {
+                return health;
+            }
+            health += heal;
+            if (health > getMaxHealth())
+            {
+                health = getMaxHealth();
+            }
+            return health;
+        }
+
+        public abstract int getStr();
+        public abstract int getDex();
+        public abstract int getInt();
+        public abstract int getVil();
+        public abstract int getMaxHealth();
+        public abstract int getAtk();
+        public abstract int getAmr();
+        public abstract int getDod();
+        public abstract double getSpd();
+
+        public int compDmg(spiritAb tg)
+        {
+            double adj = Math.Exp(0.03 * (lv - tg.lv));
+            double luk = r.NextDouble() * 0.4 + 0.8;
+            int dmg = (int)((getAtk() - tg.getAmr()) * adj * luk);
+            if (dmg < 1) dmg = 1;
+            return dmg;
+        }
+    }
+}
